7eab8e5c9807d3c2c7f05ad05b20e45515297e86
bebop831 <bebop831@thunk>
2024-08-27 19:45:18 -0700

    Added invert.py, inverts an image or directory of images given a cmd line arg

---
+++ scripts/images/invert.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageOps
+import os, sys
+
+def invert(file):
+	image = Image.open(file)
+	ImageOps.invert(image.convert('RGB')).save(file)
+	print(f"Inverted {file}...")
+
+
+
+if len(sys.argv) > 1:
+	path = sys.argv[1]
+	if os.path.isdir(path):
+		for i in os.listdir(path):
+			invert(i)
+	else:
+		invert(path)
